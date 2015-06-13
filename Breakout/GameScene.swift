9626946ca0f72725308dc29d4ec38fce057f0ddd
MathiasB <git@denbeke.be>
2015-06-13 22:33:45 +0200

    Ball and paddle

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -7,39 +7,88 @@
 //
 
 import SpriteKit
+import CoreMotion
+
+let BallCategoryName = "ball"
+let PaddleCategoryName = "paddle"
+let BlockCategoryName = "block"
+let BlockNodeCategoryName = "blockNode"
 
 class GameScene: SKScene {
+    
+    var isFingerOnPaddle = false
+    let motionManager: CMMotionManager = CMMotionManager()
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        /*
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
         myLabel.text = "Hello, World!";
         myLabel.fontSize = 65;
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
         
         self.addChild(myLabel)
+        */
+        
+        // Create a physics body that borders the screen
+        let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        // Set the friction of that physicsBody to 0
+        borderBody.friction = 0
+        // Set physicsBody of scene to borderBody
+        self.physicsBody = borderBody
+        
+        physicsWorld.gravity = CGVectorMake(0, 0)
+        
+        let ball = childNodeWithName(BallCategoryName) as! SKSpriteNode
+        ball.physicsBody!.applyImpulse(CGVectorMake(10, -10))
+        
+        motionManager.startAccelerometerUpdates()
     }
     
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
+        // Check if touch is on paddle
+        let touch = touches.first!
+        let touchLocation = touch.locationInNode(self)
+        
+        if let body = physicsWorld.bodyAtPoint(touchLocation) {
+            if body.node!.name == PaddleCategoryName {
+                print("Began touch on paddle")
+                isFingerOnPaddle = true
+            }
         }
     }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // Check whether user touched the paddle
+        if isFingerOnPaddle {
+            // Get touch location
+            let touch = touches.first!
+            let touchLocation = touch.locationInNode(self)
+            let previousLocation = touch.previousLocationInNode(self)
+            
+            // Get node for paddle
+            let paddle = childNodeWithName(PaddleCategoryName) as! SKSpriteNode
+            
+            // Calculate new position along x for paddle
+            var paddleX = paddle.position.x + (touchLocation.x - previousLocation.x)
+            
+            // Limit x so that paddle won't leave screen to left or right
+            paddleX = max(paddleX, paddle.size.width/2)
+            paddleX = min(paddleX, size.width - paddle.size.width/2)
+            
+            // Update paddle position
+            paddle.position = CGPointMake(paddleX, paddle.position.y)
+        }
     }
+    
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // Removed finger from paddle
+        isFingerOnPaddle = false
+    }
+    
+    
 }
